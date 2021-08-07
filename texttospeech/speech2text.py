419c009695c5299058873dc935fe6f7cc86d9532
boxframetech <boxframe@protonmail.com>
2021-08-07 20:03:03 +0000

    speech2text

---
+++ texttospeech/speech2text.py
@@ -0,0 +1,17 @@
+import pyttsx3, speech_recognition as sr
+
+def getSpeech():
+    recogniser = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Welcome Sir, talk to me")
+        audio = recogniser.listen(source)
+        print('done!')
+
+    try:
+        text = recogniser.recognize_google(audio)
+        print("You said ", text)
+
+    except Exception as e:
+        print(e)
+
+getSpeech()
