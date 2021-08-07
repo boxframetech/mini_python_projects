aaa0e832eeae3dab34186609ca960621a012dba7
boxframetech <boxframe@protonmail.com>
2021-08-07 18:35:54 +0000

    GUI Added

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -1,5 +1,6 @@
 import time
 import pyautogui
+import tkinter as tk
 
 def screenshot():
     file_name = int(round(time.time()*200))
@@ -8,4 +9,31 @@ def screenshot():
     screenshot_img = pyautogui.screenshot(file_name)
     screenshot_img.show()
 
-screenshot()
+
+def screenshot2():
+    file_name = int(round(time.time()*200))
+    file_name = f'{file_name}.png'
+    time.sleep(5)
+    screenshot_img = pyautogui.screenshot(file_name)
+    screenshot_img.show()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame,
+    text = 'Take a screenshot',
+    command=screenshot2
+)
+
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text="QUIT",
+    command=quit
+)
+
+close.pack(side=tk.LEFT)
+
+root.mainloop()
